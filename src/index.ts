06d05ab4798140620f3f4d6ace91cb6e85592c71
BearCurb <bigstarcat520@gmail.com>
2023-10-10 16:32:38 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import hitokotoUtil from "./util/hitokotoUtil";
+import fs from "fs";
+import path from "path";
+
+const getBot = (): Telegraf => {
+  const result = fs.readFileSync(path.join("TOKEN"), "utf-8");
+  console.log("读取TOKEN为:" + result);
+  return new Telegraf(result);
+};
+const bot: Telegraf = getBot();
+
+bot.on(message("text"), (ctx) => {
+  if (ctx.message.text === "早上好") {
+    hitokotoUtil().then((data: any) => {
+      console.log(data);
+      let message = "";
+      message += `早上好!\n\n`;
+      message += `${data.hitokoto}\t\t--${data.from}\n`;
+      ctx.reply(message);
+    });
+  }
+});
+
+bot.launch();
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
---
+++ src/util/hitokotoUtil.ts
@@ -0,0 +1,39 @@
+import http from "http";
+
+interface HitokotoType {
+  id: string;
+  hitokoto: string;
+  type: string;
+  from: string;
+  from_who: string;
+  creator: string;
+  creator_uid: string;
+  reviewer: string;
+  uuid: string;
+  commit_from: string;
+  created_at: string;
+  length: string;
+}
+
+const hitokotoUtil = (): Promise<HitokotoType> => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: "v1.hitokoto.cn",
+      port: 80,
+      path: "/",
+      agent: false,
+    };
+    http.get(options, (res) => {
+      let data = "";
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        resolve(JSON.parse(data));
+      });
+    });
+  });
+};
+
+export type { HitokotoType };
+export default hitokotoUtil;
